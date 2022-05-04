3b8d909db46df84d9ca1e43e0b6331aef705d793
Iuri Neviton <iurineviton@gmail.com>
2022-05-04 20:52:58 -0300

    get mapping

--- src/main/java/com/nevitoniuri/financesapi/controller/ResumoController.java
+++ src/main/java/com/nevitoniuri/financesapi/controller/ResumoController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class ResumoController {
 
     private final ResumoService resumoService;
 
-    @RequestMapping("/{ano}/{mes}")
+    @GetMapping("/{ano}/{mes}")
     public ResponseEntity<ResumoDoMesDTO> gerarResumoMensal(@PathVariable Integer ano, @PathVariable Integer mes,
                                                             @PageableDefault(page = 0, size = 10, sort = "descricao") Pageable pageable) {
         return ResponseEntity.ok(resumoService.gerarResumoMensal(ano, mes, pageable));
